b0859a2a71817c4575be9aaf29291f370d040ba5
Olga22S <22031992Les>
2022-03-30 12:28:28 +0300

    Initial commit

---
+++ src/main/java/pro/sky/secondcoursework/SecondCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.secondcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondCourseWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondCourseWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/secondcoursework/SecondCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.secondcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondCourseWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
